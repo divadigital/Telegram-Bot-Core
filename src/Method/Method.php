ab9ab54b8f17b677cd20c1f554b0532c53a08939
Jeremy Lim <git@jerl.im>
2018-01-30 22:32:44 +0800

    Add async sending

--- src/Method/Method.php
+++ src/Method/Method.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace KeythKatz\TelegramBotCore\Method;
 
 use KeythKatz\TelegramBotCore\BaseType;
+use GuzzleHttp\Promise\Promise;
+use TelegramBot\Api\Types\Message;
 
 abstract class Method extends BaseType
 {
@@ -28,8 +30,34 @@ abstract class Method extends BaseType
 
 	/**
 	 * Map the set parameters and send it to Telegram.
+	 * @return Telegram's response.
 	 */
-	public function send(): void
+	public function send()
+	{
+		$mappedParams = $this->map();
+
+		$curler = new \GuzzleHttp\Client(['base_uri' => $this->tgUrl]);
+
+		if (empty($this->multipart)) {
+			$response = $curler->request("POST", static::$method, [
+				"query" => $mappedParams
+			]);
+		} else {
+			$response = $curler->request("POST", static::$method, [
+				"query" => $mappedParams,
+				"multipart" => $this->multipart
+			]);
+		}
+
+		$jsonResponse = json_decode((string)$response->getBody(), true);
+		$result = $jsonResponse['result'];
+		return static::decodeResult($result);
+	}
+
+	/**
+	 * Map the set parameters and send it to Telegram asynchronously.
+	 */
+	public function sendAsync(): Promise
 	{
 		$mappedParams = $this->map();
 
@@ -46,7 +74,25 @@ abstract class Method extends BaseType
 			]);
 		}
 
-		$this->bot->addPromise($promise);
+		$processedPromise = $promise->then(function ($response) {
+			$jsonResponse = json_decode((string)$response->getBody(), true);
+			$result = $jsonResponse['result'];
+			return static::decodeResult($result);
+		});
+
+		$this->bot->addPromise($processedPromise);
+		return $processedPromise;
+	}
+
+	/**
+	 * By default, most functions return a Message. This will be overridden
+	 * for those that return other things.
+	 * @param  array  $result parsed JSON that Telegram returns
+	 * @return Message
+	 */
+	protected function decodeResult(array $result)
+	{
+		return Message::fromResponse($result);
 	}
 
 	protected function addMultipart(string $name, $resource)
